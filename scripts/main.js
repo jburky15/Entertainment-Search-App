dfcfb1ffcfe4595273bbbd9a9fe0d8e17a10e184
Joe Burkhart <jburky15@gmail.com>
2022-02-02 12:17:56 -0500

    Added comments

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
+//Shift the images of the landing page left and right based on where the user hovers their mouse
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
